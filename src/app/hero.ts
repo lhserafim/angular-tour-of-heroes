27e0b6300496cea00a032d0d541490ed510d0fec
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2021-03-30 17:30:42 -0300

    1. The Hero Editor

---
+++ src/app/hero.ts
@@ -0,0 +1,7 @@
+// Neste exemplo, o Hero foi criado como uma interface. Eu já fiz alguns exemplos onde o foi criado como uma classe
+// Descobrir a diferença
+
+export interface Hero {
+    id: number;
+    name: string;
+}
